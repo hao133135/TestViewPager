d38e8273d2c5b856480d929116ef823a4e6df7a7
hao <hao133135@163.com>
2017-06-15 10:13:20 +0800

    学员，教练包

---
+++ app/src/main/java/com/example/asus/testviewpager/driving_coach/CoachActivity.java
@@ -0,0 +1,10 @@
+package com.example.asus.testviewpager.driving_coach;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/15.
+ */
+
+public class CoachActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/asus/testviewpager/driving_student/StudentActivity.java
@@ -0,0 +1,10 @@
+package com.example.asus.testviewpager.driving_student;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/15.
+ */
+
+public class StudentActivity extends AppCompatActivity {
+}
